9045b8136eeba3d971bba5d3318435739ef952af
tripples25 <92451706+tripples25@users.noreply.github.com>
2023-05-29 02:00:29 +0500

    Плавное передвижение панели

--- Assets/Scripts/MovePanel.cs
+++ Assets/Scripts/MovePanel.cs
@@ -1,12 +1,16 @@
+using System.Collections;
 using UnityEngine;
 
 public class MovePanel : MonoBehaviour
 {
     bool isPressed;
+    private Vector3 standardPos;
+    private Vector3 pressedPos;
 
     void Start()
     {
-        
+        standardPos = this.transform.position;
+        pressedPos = standardPos -= new Vector3(0, 560, 0);
     }
     
 
@@ -14,13 +18,37 @@ public class MovePanel : MonoBehaviour
     {
         if (isPressed)
         {
-            transform.position += new Vector3(0, 360, 0);
+            StopAllCoroutines();
+            StartCoroutine(MoveUp());
         }
         else
         {
-            transform.position -= new Vector3(0, 360, 0);
+            StopAllCoroutines();
+            StartCoroutine(MoveDown());
         }
         
         isPressed = !isPressed;
+    }
+
+    public IEnumerator MoveDown()
+    {
+        var firstPos = transform.position;
+        var newPos = transform.position - new Vector3(0, 560, 0);
+        while (transform.position != newPos)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, newPos, 8);
+            yield return new WaitForSeconds(0.005f);
+        }
+    }
+    
+    public IEnumerator MoveUp()
+    {
+        var firstPos = transform.position;
+        var newPos = transform.position + new Vector3(0, 560, 0);
+        while (transform.position != newPos)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, newPos,  8);
+            yield return new WaitForSeconds(0.005f);
+        }
     }
 }
