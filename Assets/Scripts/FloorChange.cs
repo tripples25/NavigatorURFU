09f84ca79956113a8c76649787736a23c1e9a77c
Konstantin <vinnik-03@mail.ru>
2023-06-07 23:04:18 +0500

    Добавил окошко с отображением этажа

---
+++ Assets/Scripts/FloorChange.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FloorChange : MonoBehaviour
+{
+    [SerializeField] private GameObject floor;
+    private GameObject number;
+    // Start is called before the first frame update
+    void Start()
+    {
+        number = floor.transform.GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var activeSceneName = SceneManager.GetActiveScene().name;
+        if (activeSceneName == "1 этаж")
+        {
+            number.GetComponentInChildren<TMP_Text>().text = "1";
+        }
+        else if (activeSceneName == "2 этаж")
+        {
+            number.GetComponentInChildren<TMP_Text>().text = "2";
+        }
+        else if (activeSceneName == "3 этаж")
+        {
+            number.GetComponentInChildren<TMP_Text>().text = "3";
+        }
+        else
+        {
+            number.GetComponentInChildren<TMP_Text>().text = "4";
+        }
+    }
+}
---
+++ Assets/Scripts/FloorChangeColorAndLang.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloorChangeColorAndLang : MonoBehaviour
+{
+    [SerializeField] private GameObject floorInfo;
+    private GameObject number;
+    private GameObject floor;
+    bool isPressedColor;
+    bool isPressedLang;
+    // Start is called before the first frame update
+    void Start()
+    {
+        number = floorInfo.transform.GetChild(0).gameObject;
+        floor = floorInfo.transform.GetChild(1).gameObject;
+    }
+
+    public void ButtonPressedColor()
+    {
+        if (isPressedColor)
+        {
+            floorInfo.GetComponent<Image>().color = Color.white;
+            number.GetComponentInChildren<TMP_Text>().color = new Color32(157, 157, 157, 255);
+            floor.GetComponentInChildren<TMP_Text>().color = new Color32(157, 157, 157, 255);
+        }
+        else 
+        { 
+            floorInfo.GetComponent<Image>().color = new Color32(41, 41, 41, 255);
+            number.GetComponentInChildren<TMP_Text>().color = new Color32(230, 230, 230, 255);
+            floor.GetComponentInChildren<TMP_Text>().color = new Color32(230, 230, 230, 255);
+        }
+        isPressedColor = !isPressedColor;
+    }
+
+    public void ButtonPressedLang()
+    {
+        if (isPressedLang)
+        {
+            floor.GetComponentInChildren<TMP_Text>().text = "этаж";
+        }
+        else
+        {
+            floor.GetComponentInChildren<TMP_Text>().text = "floor";
+        }
+        isPressedLang = !isPressedLang;
+    }
+}
